d4086cce1fdc30212ab0c471498d93a7a2d1a83e
Alex <awintschel@gmail.com>
2018-01-21 22:51:03 -0800

    completed script, all post templates, and solved post display issue

--- create-365-posts.sh
+++ create-365-posts.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# this script creates a post file for every day of the year, giving it a unique date in the title and some content to meet jekyll post format
+# this script creates a post file for every day of the year, giving it a unique date in the title, front matter, content
 
-for i in `seq 0 365`; # this loop is a simple sequence looping from 0 to 365
+for i in `seq 1 365`; # this loop is a simple sequence looping from 0 to 365
 do
-	touch $(date -d "+$i day" "+%Y-%m-%d"-"post.md"); # expands the date command into the title of each text file to meet jekyll post format
-	echo -e "---\nlayout:post\ntitle:post\ndate:"$(date -d  "+$i day" "+%Y-%m-%d")"\n---\n\nThis is some sample content\n"
-	>> $(date -d "+$i day" "+%Y-%m-%d"-"post.md"); # redirects echo into each post file in jekyll formatting
+	touch $(date -d "+$i day" "+%Y-%m-%d"-"newpost.md"); # expands the date command into the title of each text file to meet jekyll post format
+	echo -e "---\nlayout: post\ntitle: \"newpost\"\ndate: "$(date -d  "+$i day" "+%Y-%m-%d %H:%M:%S %z")"\ncategories: linux\n---\n\nThis is some sample content\n" >> $(date -d "+$i day" "+%Y-%m-%d"-"newpost.md"); # redirects echo to make up yaml front matter
 done
